40eb68d94b9e1ab57cbce7b417ea98d533db41ea
Axiileszko <leszkoalexa01@gmail.com>
2025-03-05 12:15:29 +0100

    Added camera dragging inside the game

---
+++ Zombicide/Assets/Controller/CameraDrag.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class CameraDrag : MonoBehaviour
+{
+    private Vector3 dragOrigin;
+    private float dragSpeed = 35f;
+    private Vector2 panLimitX = new Vector2(-20f, 20f);
+    private Vector2 panLimitZ = new Vector2(-40f, 0f);
+    private Vector3 resetCameraPosition;
+    private bool isDragging = false;
+
+    void Start()
+    {
+        resetCameraPosition = Camera.main.transform.position;
+    }
+    void Update()
+    {
+        HandleMouseDrag();
+        HandleResetCamera();
+    }
+    void HandleMouseDrag()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            isDragging = true;
+        }
+        if (Input.GetMouseButton(0) && isDragging)
+        {
+            Vector3 difference = dragOrigin - Input.mousePosition;
+            dragOrigin = Input.mousePosition;
+
+            Vector3 move = new Vector3(difference.x * dragSpeed * Time.deltaTime, 0, difference.y * dragSpeed * Time.deltaTime);
+            MoveCamera(move);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+    }
+    void HandleResetCamera()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Camera.main.transform.position = resetCameraPosition;
+        }
+    }
+    void MoveCamera(Vector3 move)
+    {
+        Vector3 newPosition = Camera.main.transform.position + move;
+        newPosition.x = Mathf.Clamp(newPosition.x, panLimitX.x, panLimitX.y);
+        newPosition.z = Mathf.Clamp(newPosition.z, panLimitZ.x, panLimitZ.y);
+        Camera.main.transform.position = newPosition;
+    }
+
+}
